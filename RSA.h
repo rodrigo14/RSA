9f8366763705e5cac74ab7e1aa8e46b536fc4116
Rodrigo <rodrigosg2000@hotmail.com>
2014-05-23 09:26:32 -0300

    alteração dos parâmetros

--- RSA.h
+++ RSA.h
@@ -4,13 +4,14 @@
 using namespace std;
 using namespace NTL;
 
-ZZ find_large_prime(long nbits);
-ZZ mdc(ZZ &p, ZZ &q);
-ZZ privatekey_generator(ZZ p, ZZ q);
-ZZ publickey_generator(ZZ p, ZZ q);
 string encrypt(string text);
 string decrypt(string text);
-
+ZZ generate_large_prime(long nbits);
 ZZ generate_long_number(long nbits);
+ZZ publickey_generator(ZZ p, ZZ q);
+ZZ privatekey_generator(ZZ p, ZZ q);
+ZZ mdc(ZZ &p, ZZ &q);
+ZZ modular_exponential(const ZZ &a, const ZZ &b, const ZZ &n);
+
 
 #endif
